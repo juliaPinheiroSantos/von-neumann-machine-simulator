88dda9b65db5978d66ced9cf8fbb7415d7589504
Julia Pinheiro Santos <julia.pinheiro@estudantes.ifg.edu.br>
2026-05-03 22:55:09 -0300

    Recuperando main.c e aplicando alterações de lógica

---
+++ src/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+unsigned int mbr; 
+unsigned short mar, pc, imm; 
+unsigned char ir, ro0, ro1, e, l, g; 
+unsigned short reg[8]; 
+unsigned char memoria[256];
+
+
+void fetch();
+void decode();
+
+int main(){
+
+
+    return 0;
+}
+
+
+void fetch(){
+    mar = pc;
+    mbr = memoria[mar];
+    mar++;
+    mbr = (mbr << 8) + memoria[mar];
+    mar++;
+    mbr = (mbr << 8) + memoria[mar];
+
+}
+
+void decode(){
+    ir = mbr >> 19;
+    if(ir >= 0b00000 && ir <= 0b00001){
+        return
+    }
+
+    if(ir >= 0b01110 && ir <= 0b10100){
+        mbr = mbr << 8;
+        pc = mbr >> 7;
+    }
+
+    if(ir >= 0b10111 && ir <= 0b11101){
+        ro0 = (mbr << 13) >> 29;
+        mbr = mbr << 8;
+        imm = mbr >> 7;
+    }
+
+
+}
+
+
